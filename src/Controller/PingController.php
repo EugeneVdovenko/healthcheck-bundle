70eb7f9a592d3868e4615e8612a0525853d21d40
Evgenii Vdovenko <evgenii@openstudio.su>
2024-11-11 02:36:10 +0300

    Init

---
+++ src/Controller/PingController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenStudio\HealthcheckBundle\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[AsController]
+#[Route(path: '/healthcheck/ping', name: 'healthcheck_ping', methods: [Request::METHOD_GET])]
+class PingController
+{
+    public function __invoke(): Response
+    {
+        return new JsonResponse("pong");
+    }
+}
---
+++ src/OpenStudioHealthcheckBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenStudio\HealthcheckBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class OpenStudioHealthcheckBundle extends AbstractBundle
+{
+
+}
