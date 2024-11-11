eb232c5e7ade4e5a7f1caf354328ab2a89aab636
Evgenii Vdovenko <evgenii@openstudio.su>
2024-11-11 03:39:19 +0300

    Fix bundle config

--- src/OpenStudioHealthcheckBundle.php
+++ src/OpenStudioHealthcheckBundle.php
@@ -14,4 +14,9 @@ class OpenStudioHealthcheckBundle extends AbstractBundle
     {
         $container->import('../config/services.yaml');
     }
+
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
 }
